00440cf333d999246a379503a3519d8827982faf
khai <khai@keyta.id>
2021-10-29 00:23:48 +0700

    add message box and send message realtime

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const socketio =  require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -20,19 +22,33 @@ io.on('connection', (socket) => {
   console.log('We have a new connection!!');
 
 	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
 
-		const error = false;
+		if(error) return callback(error);
 
-		if(error) {
-			callback({ error: 'error'});
-		}
+		socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room ${user.room}`})
+		socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined`})
+		
+		socket.join(user.room);
 
+		io.to(user.room).emit('roomData', { room: user.room , users: getUserInRoom(user.room)})
+
+		callback();
+	})
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message});
+		io.to(user.room).emit('roomData', { room: user.room, text: getUserInRoom(user.room)});
+
+		callback();
 	})
 
 	socket.on('disconnect', () => {
 		console.log('User had left');
 	})
-})
+});
 
 app.use(cors());
 app.use(router);
